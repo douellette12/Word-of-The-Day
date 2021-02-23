07ad313534c167b690165169b3191e45a074b18f
douellette12 <39159681+douellette12@users.noreply.github.com>
2021-02-23 08:43:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from wod import get_wod, get_wod_info, send_email, db_conn, insert_row
 
 def main():
     wod = get_wod()
+    short_def, pronunciation, word_type = get_wod_info(wod)
     conn, cursor = db_conn()
     insert_row(conn, cursor, datetime.date.today(), wod, word_type, pronunciation, short_def)
-    short_def, pronunciation, word_type = get_wod_info(wod)
     send_email(wod, word_type, pronunciation, short_def)
 
 
